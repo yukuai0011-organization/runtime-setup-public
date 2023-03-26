a52aa25969f8795e9ac9288150a3f161649ee541
yukuai0011 <12008259+yukuai0011@users.noreply.github.com>
2023-03-26 21:01:46 +1100

    save
    Signed-off-by: yukuai0011 <12008259+yukuai0011@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 pip3 install --upgrade pip
-pip3 install pylint -U --user --force-reinstall
-pip3 install psycopg2 -U --user --force-reinstall
-pip3 install SQLAlchemy -U --user --force-reinstall
-pip3 install pandas -U --user --force-reinstall
-pip3 install Flask -U --user --force-reinstall
-pip3 install pymongo -U --user --force-reinstall
-pip3 install dnspython -U --user --force-reinstall
-pip3 install geopandas -U --user --force-reinstall
-pip3 install GeoAlchemy2 -U --user --force-reinstall
-pip3 install Shapely -U --user --force-reinstall
-pip3 install Fiona -U --user --force-reinstall
-pip3 install ipykernel -U --user --force-reinstall
-pip3 install numba -U --user --force-reinstall
-pip3 install pyspark -U --user --force-reinstall
-pip3 install pg8000 -U --user --force-reinstall
-pip3 install psycopg -U --user --force-reinstall
-pip3 install asyncpg -U --user --force-reinstall
+pip3 install pylint
+pip3 install psycopg2
+pip3 install SQLAlchemy
+pip3 install pandas
+pip3 install Flask
+pip3 install pymongo
+pip3 install dnspython
+pip3 install geopandas
+pip3 install GeoAlchemy2
+pip3 install Shapely
+pip3 install Fiona
+pip3 install ipykernel
+pip3 install numba
+pip3 install pyspark
+pip3 install pg8000
+pip3 install psycopg
+pip3 install asyncpg
 Rscript setup.r
